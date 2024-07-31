94066bfb556a671831ad69a47491717103e89ece
LizManchego <karinamanchego0416@gmail.com>
2024-07-31 15:36:16 -0500

    Glosario / Referencias / Compl./ Creditos / Menu

--- src/config/global.js
+++ src/config/global.js
@@ -36,28 +36,57 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Creatividad',
         desarrolloContenidos: true,
-        subMenu: [
-          {
-            numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
-            hash: 't_1_1',
-          },
-        ],
       },
 
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Definiciones de creatividad',
         desarrolloContenidos: true,
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'El proceso creativo',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Técnicas creativas',
+            hash: 't_3_1',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Estrategia creativa',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema5',
+        numero: '5',
+        titulo: 'Desarrollo de la estrategia',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '5.1',
+            titulo: 'Objetivos',
+            hash: 't_5_1',
+          },
+          {
+            numero: '5.2',
+            titulo: 'Target comunicacional',
+            hash: 't_5_2',
+          },
+          {
+            numero: '5.3',
+            titulo: 'El concepto creativo',
+            hash: 't_5_3',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -107,32 +136,112 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'La Creatividad',
+      referencia:
+        'Ecosistema de recursos educativos digitales SENA. 2020. La Creatividad.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=ivz15d1vPI0',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'La estrategia creativa',
+      referencia:
+        'Ecosistema de recursos educativos digitales SENA.  2020.  Proceso creativo.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=gyWNLNeB1Jc',
+    },
+    {
+      tema: 'Propuesta de valor.',
+      referencia: 'Nestle. 2011. Propuesta de valor.',
+      tipo: 'Infografía',
+      descarga: '/downloads/propuestadevalor_infografia.pdf',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Aviso',
+      significado:
+        'En términos generales, un aviso, refiere a aquel anuncio o noticia que una persona tiene para darle a otra o bien, aquel que está destinado a un público mucho más amplio y que puede referir a cualquier situación plausible de comunicarse.  Casi siempre, si se trata de este último caso, los medios de comunicación masiva serán quienes porten el aviso en cuestión. ',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: '<i>Branding<i>',
+      significado:
+        'Hace referencia al proceso de creación de valor de marca (<i>brand equity</i>) mediante la administración estratégica del conjunto total de activos y pasivos vinculados en forma directa o indirecta al nombre o símbolo (isotipo) que identifican a la marca influyendo en el valor suministrado; tanto al cliente como a la empresa oferente; por un producto o servicio, incrementando o reduciendo según el caso. ',
+    },
+    {
+      termino: '<i>Briefing</i>',
+      significado:
+        'Conjunto de datos sobre la empresa, sus objetivos, su estrategia... que sirven de base al consultor o diseñador para plantear la comunicación corporativa. ',
+    },
+    {
+      termino: 'Canal',
+      significado:
+        'En teoría de la comunicación, camino técnico o fisiológico que recorre un mensaje desde el emisor hasta el receptor. ',
+    },
+    {
+      termino: 'Comunicación',
+      significado:
+        'Término que deriva del latín <i>commune</i> y del sufijo ie-, similar a fie-, que significa hacer; es decir, realizar la transmisión de información o de conocimiento entre una persona y otra del modo más exacto posible. ',
+    },
+    {
+      termino: 'Diseño gráfico',
+      significado:
+        'Es una actividad intelectual, técnica y creativa involucrada no solamente con la producción de imágenes sino con el análisis, la organización y los métodos de presentación de soluciones visuales a los problemas de comunicación. ',
+    },
+    {
+      termino: 'Información',
+      significado:
+        'Se entiende comúnmente por información la noticia; es decir, el mensaje ligado al contenido de una cosa significada. ',
+    },
+    {
+      termino: 'Mensaje',
+      significado:
+        'Serie organizada de signos según un código, con la intención de comunicar. ',
+    },
+    {
+      termino: 'Mnemotécnico',
+      significado:
+        'Es un sistema sencillo de código para recordar aspectos fácilmente (señales, números, letras, datos, nombres, entre otros). ',
+    },
+    {
+      termino: '<i>Teaser</i>',
+      significado:
+        'Es conocido como una técnica que se utiliza al momento de lanzar, ya sea un producto o un servicio. Su principal función es generar intriga en el público. ',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Castelló, A. Pino, C. (2019). De la Publicidad a la Comunicación Persuasiva. Esic Editorial. Consumer truth. (2020). Técnicas de Insights. https://consumer-truth.com.pe/tecnicas-de-insights/',
+      link: 'https://consumer-truth.com.pe/tecnicas-de-insights/',
+    },
+    {
+      referencia:
+        'Davara, A. M. (1992). Estrategias de Comunicación en Marketing. Edit. DOSSAT S.A. Fundación Neuronilla para la Creatividad y la Innovación. (2020). Técnicas de Creatividad para la Innovación. https://neuronilla.com/desarrolla-creatividad/tecnicas-creatividad/',
+      link:
+        'https://neuronilla.com/desarrolla-creatividad/tecnicas-creatividad/',
+    },
+    {
+      referencia:
+        'Gerard, J. P. (2015). Imagina lo Imposible: Manual Práctico & Caja de Herramientas para la Innovación. 2015. Ril Editores, Editorial Universidad Católica de la Santísima Concepción. Publicidad y Promoción. (2010). El Proceso Creativo. https://publicidadypromocion2010.wordpress.com/2010/11/08/el-proceso-creativo/',
+      link:
+        'https://publicidadypromocion2010.wordpress.com/2010/11/08/el-proceso-creativo/',
+    },
+
+    {
+      referencia:
+        'Rajadell, M. (2019). Creatividad. Emprendimiento y Mejora Continua. Editorial Reverté. Reimers Design. (s.f.). Pequeño Diccionario del Diseñador. Consultado el 27 de mayo de 2020. https://losqueno.com/wp-content/uploads/2015/04/DICCIONARIO-DISE--O.pdf',
+      link:
+        'https://losqueno.com/wp-content/uploads/2015/04/DICCIONARIO-DISE--O.pdf',
+    },
+    {
+      referencia:
+        'Roig, F. A. (2011). La estrategia creativa: Relaciones entre concepto e idea. Ediciones Infinito. Vila, F. (2013). Comunicación Estratégica. Editorial UOC. https://losqueno.com/wp-content/uploads/2015/04/DICCIONARIO-DISE--O.pdf',
+      link:
+        'https://losqueno.com/wp-content/uploads/2015/04/DICCIONARIO-DISE--O.pdf',
+    },
+    {
+      referencia:
+        'Vilajoana, A., Jiménez, M., González, Z. y Vila, J. (2014). ¿Cómo diseñar una campaña de publicidad? Editorial UOC. ',
     },
   ],
   creditos: [
@@ -145,9 +254,9 @@ export default {
           centro: 'Dirección General',
         },
         {
-          nombre: 'Olga Constanza Bermúdez Jaimes',
+          nombre: 'Claudia Johanna Gómez Pérez',
           cargo: 'Responsable de Línea de Producción',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Regional Santander - Centro Agroturístico',
         },
       ],
     },
@@ -155,9 +264,41 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+          nombre: 'Rafael Neftalí Lizcano Reyes',
+          cargo: 'Asesor pedagógico',
+          centro:
+            'Regional Santander - Centro Industrial del Diseño y la Manufactura ',
+        },
+        {
+          nombre: 'Leidy Johana Velásquez Hernández',
+          cargo: 'Experta temática',
+          centro: 'Regional Antioquía - Diseño, Confección y Moda ',
+        },
+        {
+          nombre: 'Paola Angélica Castro Salazar',
+          cargo: 'Experta temática',
+          centro: 'Regional Antioquía - Diseño, Confección y Moda ',
+        },
+        {
+          nombre: 'Paula Andrea Taborda Ortiz',
+          cargo: 'Diseñadora instruccional',
+          centro: 'Regional Distrito Capital - Centro de Diseño y Metrología ',
+        },
+        {
+          nombre: 'Carolina Coca Salazar',
+          cargo: 'Revisora metodológica y pedagógica',
+          centro: 'Regional Distrito Capital - Centro de Diseño y Metrología ',
+        },
+        {
+          nombre: 'Jhon Jairo Rodríguez Pérez',
+          cargo: 'Diseñador y evaluador instruccional',
+          centro:
+            'Regional Distrito Capital - Centro para la Industria de la Comunicación Gráfica ',
+        },
+        {
+          nombre: 'Erika Fernanda Mejía Pinzón',
+          cargo: 'Evaluadora Instruccional',
+          centro: 'Regional Santander - Centro Agroturístico',
         },
       ],
     },
@@ -165,24 +306,14 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Diseñador de Contenidos Digitales',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+          nombre: 'Yuly Andrea Rey Quiñonez',
+          cargo: 'Diseñadora web',
+          centro: 'Regional Santander - Centro Agroturístico',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Desarrollador Fullstack',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
-        },
-        {
-          nombre: 'Nombre',
-          cargo: 'Animador y Producción audiovisual',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
-        },
-        {
-          nombre: 'Nombre',
-          cargo: 'Actividad Didáctica',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+          nombre: 'Lizeth Karina Manchego Suarez',
+          cargo: 'Desarrolladora <i>Full-Stack</i>',
+          centro: 'Regional Santander - Centro Agroturístico',
         },
       ],
     },
@@ -190,24 +321,19 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Luis Gabriel Urueta Alvarez',
-          cargo: 'Validador de Recursos Educativos Digitales',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+          nombre: 'Yineth Ibette Gonzalez Quintero',
+          cargo: 'Validadora de Recursos Educativos Digitales',
+          centro: 'Regional Santander - Centro Agroturístico',
         },
         {
-          nombre: 'Jaime Hernán Tejada Llano',
-          cargo: 'Validador de Recursos Educativos Digitales',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+          nombre: 'Laura Paola Gelvez Manosalva',
+          cargo: 'Validadora de Recursos Educativos Digitales',
+          centro: 'Regional Santander - Centro Agroturístico',
         },
         {
-          nombre: 'Daniel Ricardo Mutis Gómez',
-          cargo: 'Evaluador para contenidos inclusivos y accesibles',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
-        },
-        {
-          nombre: 'Margarita Marcela Medrano Gómez',
-          cargo: 'Evaluador para contenidos inclusivos y accesibles',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+          nombre: 'Erika Fernanda Mejía Pinzón',
+          cargo: 'Evaluadora para contenidos inclusivos y accesibles',
+          centro: 'Regional Santander - Centro Agroturístico',
         },
       ],
     },
@@ -218,25 +344,4 @@ export default {
     creativeCommons:
       'Licencia creative commons CC BY-NC-SA<br><a href="https://creativecommons.org/licenses/by-nc-sa/2.0/" target="_blank">ver licencia</a>',
   },
-  // creditosInicio: [
-  //   {
-  //     titulo: 'En alianza',
-  //     contenido: [
-  //       require('@/assets/template/logo-sena-naranja.svg'),
-  //       require('@/assets/template/mintic.jpg'),
-  //       require('@/assets/template/minsalud.jpg'),
-  //     ],
-  //   },
-  //   {
-  //     titulo: 'En compañía',
-  //     contenido: [
-  //       require('@/assets/template/presidencia.jpg'),
-  //       require('@/assets/template/ecopetrol.jpg'),
-  //     ],
-  //   },
-  //   {
-  //     titulo: 'Una iniciativa',
-  //     contenido: [require('@/assets/template/santander.jpg')],
-  //   },
-  // ],
 }
